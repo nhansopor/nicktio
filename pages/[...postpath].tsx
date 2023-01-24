d5038aeeb06469a9e9482cedebfdd0e95ad89091
brochhay <74852547+Brochhay@users.noreply.github.com>
2023-01-24 12:43:16 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -84,18 +84,20 @@ const Post: React.FC<PostProps> = (props) => {
 			<Head>
 
 				<link rel="canonical" href={`https://${host}/${path}`} />
-
+				<meta property="og:title" content={post} />
+                <meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
-				
 
+
+				
 			</Head>
 			<div className="post-container">
-				<h1>{post.any}</h1>
+				<h1>{post.jinny}</h1>
 				<img
 					src={post.featuredImage.node.sourceUrl}
 					alt={post.featuredImage.node.post}
