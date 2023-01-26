4a281bdb83d0f45b36361de18cd77502928ded7f
brochhay <74852547+Brochhay@users.noreply.github.com>
2023-01-26 14:56:27 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -28,11 +28,11 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			post(id: "/${path}/", idType: URI) {
 				id
 				excerpt
-				
+				tittle
 				link
 				dateGmt
 				modifiedGmt
-				
+				content
 				author {
 					node {
 						name
