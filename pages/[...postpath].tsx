6b9752b32700ab39e35a5885e75aabc98fd86f01
brochhay <74852547+Brochhay@users.noreply.github.com>
2023-01-26 15:22:13 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -41,7 +41,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 				featuredImage {
 					node {
 						sourceUrl
-						
+						altText
 					}
 				}
 			}
@@ -82,29 +82,27 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-			
+				<meta property="og:title" content={post.title} />
 				<link rel="canonical" href={`https://${host}/${path}`} />
 				<meta property="og:description" content={removeTags(post.excerpt)} />
 				<meta property="og:url" content={`https://${host}/${path}`} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={"jinnytv"} />
+				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
-					property="og:image"
-					content={post.featuredImage }
-					
+					property="og:image:alt"
+					content={post.featuredImage.node.altText || post.title}
 				/>
-				<title></title>;
-			</Head >
-			
+				<title>{post.title}</title>
+			</Head>
 			<div className="post-container">
-				<h1>{}</h1>
+				<h1>{post.title}</h1>
 				<img
 					src={post.featuredImage.node.sourceUrl}
-					alt={post.featuredImage}
+					alt={post.featuredImage.node.altText || post.title}
 				/>
 				<article dangerouslySetInnerHTML={{ __html: post.content }} />
 			</div>
